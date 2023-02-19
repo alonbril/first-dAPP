442494ebea62dda6c647770c2a1caf866d113901
alonbril <alonbril5@gmail.com>
2023-02-19 18:44:17 +0200

    edited

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -3,10 +3,23 @@ pragma solidity ^0.5.0;
 contract Election {
     // Store candidate
     // Read candidate
-    string public candidate;
+    struct Candidate {
+        uint256 id;
+        string name;
+        uint256 voteCount;
+    }
+
+    mapping(uint256 => Candidate) public candidates;
+    uint256 public candidatesCount;
 
     // Constructor
     constructor() public {
-        candidate = "candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
